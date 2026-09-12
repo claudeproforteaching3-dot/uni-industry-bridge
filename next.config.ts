42440ba8d830a71320823369c5e51bafa1ae470c
Seavpav <claudeproforteaching3@gmail.com>
2026-09-12 09:34:01 +0700

    Fix GitHub Pages paths

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = 'uni-industry-bridge';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: isGithubActions ? `/${repo}` : '',
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
 };
 
 export default nextConfig;
