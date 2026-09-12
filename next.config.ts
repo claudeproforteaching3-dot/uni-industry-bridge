08f7624ee0c7fb84e7b9ab1b76960712150eadbb
Seavpav <claudeproforteaching3@gmail.com>
2026-09-12 10:00:54 +0700

    Hardcode basePath

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let repo = 'uni-industry-bridge';
-
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isGithubActions ? `/${repo}` : '',
-  assetPrefix: isGithubActions ? `/${repo}/` : '',
+  basePath: '/uni-industry-bridge',
+  assetPrefix: '/uni-industry-bridge',
 };
 
 export default nextConfig;
